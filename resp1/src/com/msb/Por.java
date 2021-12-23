a656cc7a6212d2500599c442d4737801aaf6e94b
xixifeng <chindxxf@126.com>
2021-12-23 17:30:43 +0800

    修改了Por中的数据

---
+++ resp1/src/com/msb/Por.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: xixifeng
+ * @Date: 2021/12/23 - 12 - 23 - 17:27
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Por {
+    private String name;
+    private double height;
+}
